25c52717b5f4a78121a0f99126b9f9efa5cd607d
noel <noelbclarion@gmail.com>
2025-01-28 00:18:17 +0800

    Initial script for fetch quotes

---
+++ JavascriptProjects/Quote-Generator/script.js
@@ -0,0 +1,16 @@
+let apiQuotes = [];
+// Get Quotes From API.
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes[12]);
+    } catch (er) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
