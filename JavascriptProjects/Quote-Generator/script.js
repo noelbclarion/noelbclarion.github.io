fa548c7b536337216529b92309950ab6882121d6
Noel Clarion <noel.clarion@verias.com>
2025-03-02 05:19:12 +0800

    Creating a Loader with Javascript

--- JavascriptProjects/Quote-Generator/script.js
+++ JavascriptProjects/Quote-Generator/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     
@@ -22,12 +36,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    
+    // Set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API.
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try{
